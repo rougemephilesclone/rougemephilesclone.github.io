0f46d3a6d0eebbc2bd1547a696c85fb88e32ff5d
unknown <lf13cm@LABS-WP029.campus.brocku.local>
2015-11-12 18:34:36 -0500

    Week 8 JavaScript lesson.

---
+++ theJavaScripts.js
@@ -0,0 +1,18 @@
+
+function changeText(){document.getElementById("para") .innerHTML = "Monkeys"}
+
+document.write("<p> I love cats. </p>")
+
+var numOfMonkeys;
+
+numOfMonkeys = 5;
+
+numOfMonkeys = numOfMonkeys + 5;
+
+numOfMonkeys = numOfMonkeys * 5;
+
+numOfMonkeys = numOfMonkeys / 10;
+
+numOfMonkeys = numOfMonkeys - 10;
+
+document.write(numOfMonkeys);
